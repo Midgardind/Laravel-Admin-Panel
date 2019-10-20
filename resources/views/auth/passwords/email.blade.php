714a954884982505596258fae930b9472420cce3
Charlie Moyer|Midgard Industries <charlie@midgardind.com>
2019-10-20 16:41:26 -0700

    fixed padding on auth views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
+                <div class="card-body py-3">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
